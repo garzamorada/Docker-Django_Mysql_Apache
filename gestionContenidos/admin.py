c8ad7ecfa5852a8906739d9ef401828a298b2821
Andres Allievi <garzamorada@gmail.com>
2021-12-06 00:03:25 -0300

    manejador de archivos

--- gestionContenidos/admin.py
+++ gestionContenidos/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.admin.decorators import display
+from gestionContenidos.models import Peliculas
+
+class PeliculasAdmin(admin.ModelAdmin):
+    list_display=('titulo','categoria','imagen','fecha','youtube')
+    search_fields=("titulo","descripcion",)
+    list_filter=("categoria","fecha")
+    date_hierarchy="fecha"
 
 # Register your models here.
+admin.site.register(Peliculas,PeliculasAdmin)
--- gestionContenidos/models.py
+++ gestionContenidos/models.py
@@ -1,11 +1,27 @@
+import os
+from django.conf import settings
 from django.db import models
 
 class Peliculas(models.Model):
+        marvel = 'marvel'
+        dc = 'dc'
+        fox = 'fox'
+        wb = 'wb'
+        startrek = 'startrek'
+        starwars = 'starwars'
+        ListaCatrgorias=[
+                (marvel, 'Marvel Studios'),
+                (dc, 'DC Cinematic Universe'),
+                (fox, '20 Century Fox'),
+                (wb, 'Warner Movies'),
+                (startrek, 'Star Treck Univese'),
+                (starwars, 'Star Wars Universe'),
+        ]
         id=models.AutoField(primary_key=True)
         fecha = models.DateTimeField(auto_now_add=True)
         titulo=models.CharField(max_length=250)
-        imagen=models.CharField(max_length=100)
-        url=models.CharField(max_length=250)
-        categoria=models.CharField(max_length=100)
+        url=models.CharField(max_length=250,default='../pages/player.html',blank=True)
+        categoria=models.CharField(max_length=100,choices=ListaCatrgorias)
         youtube=models.CharField(max_length=100)
-        descripcion=models.TextField()
+        descripcion=models.TextField(blank=True)
+        imagen=models.ImageField(upload_to='images/',default=None)
